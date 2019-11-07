a6f2ffede08928681a91c78be7946742a7d8c9a3
Kshitij Deota <kshitij@kdpixels.com>
2019-11-07 09:18:00 +0530

    update to nav, work and about page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,35 @@
 window.onscroll = function() {stickyHeader()};
 var header = document.getElementById("kd-page-header");
 function stickyHeader() {
-	if (document.body.contains(header)) {
-		var sticky = header.offsetTop;
-		if (window.pageYOffset > sticky) {
-			header.classList.add("sticky");
-		} else {
-			header.classList.remove("sticky");
-		}
-	}
+    if (document.body.contains(header)) {
+        var sticky = header.offsetTop;
+        if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    }
 }
 
 var bunsPrimary = $("#kd-buns-primary");
 var bunsSecondary = $("#kd-buns-secondary");
 
 $(document).ready(function() {
-	$("#kd-buns-primary").click(function() {
-		$(this).toggleClass('open');
-		$("html, body").toggleClass('noscroll');
-		$(".kd-nav-primary").slideToggle();
-		$(".kd-branding .kd-nav-social").fadeToggle();
-	});
+    $("#kd-buns-primary").click(function() {
+        $(this).toggleClass('open');
+        $("html, body").toggleClass('noscroll');
+        $(".kd-nav-primary").slideToggle();
+        $(".kd-branding .kd-nav-social").fadeToggle();
+    });
 
-	$("#kd-buns-secondary").click(function() {
-		$(this).toggleClass('open');
-		$("html, body").toggleClass('noscroll');
-		$(".kd-nav-secondary").slideToggle();
-	});
+    $("#kd-buns-secondary").click(function() {
+        $(this).toggleClass('open');
+        $("html, body").toggleClass('noscroll');
+        $(".kd-nav-secondary").slideToggle();
+    });
 
-	$(".kd-footer h3").click(function() {
-		$(this).next('nav').slideToggle();
-		$(this).toggleClass('rotate45');
-	});
-});
-
-var elem = document.querySelector('.kd-portfolio-wrapper');
-var msnry = new Masonry( elem, {
-  // options
-  itemSelector: '.kd-portfolio',
-  gutter: 30,
-  columnWidth: '.kd-portfolio-sizer',
-  percentPosition: true
+    $(".kd-footer h3").click(function() {
+        $(this).next('nav').slideToggle();
+        $(this).toggleClass('rotate45');
+    });
 });
